b0218a0063b11384e3e5210a214eb1f38be3ce11
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2023-12-11 00:29:31 +0530

    day01 part 2

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -17,7 +17,10 @@ func main() {
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
 	result := solvePuzzle(lines)
-	fmt.Println("Solution:", result)
+	fmt.Println("Solution 1a:", result)
+
+	result2 := solvePuzzle2(lines)
+	fmt.Println("Solution 1b:", result2)
 }
 
 func solvePuzzle(data []string) int {
@@ -28,6 +31,42 @@ func solvePuzzle(data []string) int {
 	}
 
 	return result // Change this to the actual solution
+}
+
+func solvePuzzle2(data []string) int {
+	result := 0
+
+	for _, line := range data {
+		line = replaceStringByInt(line)
+		result += (10 * getFirstNumber(line)) + getLastNumber(line)
+	}
+
+	return result
+}
+
+func replaceStringByInt(line string) string {
+	stringInt := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+	for idx, s := range stringInt {
+		conv := strconv.Itoa(idx + 1)
+		firstChar := string(s[0])
+		lastChar := string(s[len(s)-1])
+		replace := firstChar + conv + lastChar
+		line = strings.ReplaceAll(line, s, replace)
+	}
+	// strings.ReplaceAll to find all occurrences of the current string s in the line and replaces them with
+	// the replace string. This updates the line variable with the replacements
+
+	return line
 }
 
 func getFirstNumber(line string) int {
