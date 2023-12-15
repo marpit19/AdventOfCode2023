a33929e7f828475b5c67de18aa4da18f141cb3dc
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2023-12-15 13:12:33 +0530

    day05 part1

---
+++ cmd/day05/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strings"
+)
+
+type Columns struct {
+	Seeds                 []int64
+	SeedToSoil            []Mapping
+	SoilToFertilizer      []Mapping
+	FertilizerToWater     []Mapping
+	WaterToLight          []Mapping
+	LightToTemperature    []Mapping
+	TemperatureToHumidity []Mapping
+	HumidityToLocation    []Mapping
+}
+
+type Mapping struct {
+	RangeLen              int64
+	SourceRangeStart      int64
+	DestinationRangeStart int64
+}
+
+func main() {
+	file, err := os.Open("../../inputs/day05.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	var columns Columns
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	line := scanner.Text()
+	line = line[7:] // "seeds: "
+
+	for _, seed := range strings.Split(line, " ") {
+		var value int64
+		fmt.Sscanf(seed, "%d", &value)
+		columns.Seeds = append(columns.Seeds, value)
+	}
+
+	scanner.Scan()
+	scanner.Scan() // "seed-to-soil map:"
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.SeedToSoil = append(columns.SeedToSoil, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "soil-to-fertilizer: "
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.SoilToFertilizer = append(columns.SoilToFertilizer, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "fertilizer-to-water: "
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.FertilizerToWater = append(columns.FertilizerToWater, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "water-to-light: "
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.WaterToLight = append(columns.WaterToLight, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "light-to-temperature: "
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.LightToTemperature = append(columns.LightToTemperature, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "temperature-to-humidity:"
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.TemperatureToHumidity = append(columns.TemperatureToHumidity, Mapping{length, source, destination})
+	}
+
+	scanner.Scan() // "humidity-to-location:"
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		var length, source, destination int64
+		fmt.Sscanf(line, "%d %d %d", &destination, &source, &length)
+		columns.HumidityToLocation = append(columns.HumidityToLocation, Mapping{length, source, destination})
+	}
+
+	result := solvePuzzle(columns)
+	fmt.Println("Solution5a:", result)
+}
+
+func solvePuzzle(data Columns) int64 {
+	var res int64 = math.MaxInt64
+
+	for _, seed := range data.Seeds {
+		soil := adjustValue(seed, data.SeedToSoil)
+		fertilizer := adjustValue(soil, data.SoilToFertilizer)
+		water := adjustValue(fertilizer, data.FertilizerToWater)
+		light := adjustValue(water, data.WaterToLight)
+		temperature := adjustValue(light, data.LightToTemperature)
+		humidity := adjustValue(temperature, data.TemperatureToHumidity)
+		location := adjustValue(humidity, data.HumidityToLocation)
+
+		if location<res{
+			res=location
+		}
+	}
+
+	return res
+}
+
+func adjustValue(n int64, mappings []Mapping) int64 {
+	for _, mapping := range mappings {
+		if (mapping.SourceRangeStart <= n) && (n <= (mapping.SourceRangeStart + mapping.RangeLen)) {
+			return mapping.DestinationRangeStart + (n - mapping.SourceRangeStart)
+		}
+	}
+	return n
+}
+
